85d3d09d7d346111b23df43a27183436fe0128ea
kurikei <kurikei@gmail.com>
2016-11-06 18:33:25 +0900

    auto commit 2016-11-06T18:33:25+0900

--- auto_commit_push.sh
+++ auto_commit_push.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Crontab Usage:
-# 0 */3 * * * $HOME/.homesick/repos/user_conf/auto_commit_push.sh > /dev/null
+# 0 */3 * * * $HOME/.homesick/repos/user_conf/auto_commit_push.sh >> /tmp/git_auto_commit_push.txt.`date +%Y%m%d` 2>&1
 GIT_DIR=$(cd $(dirname $0) && pwd)
 
 cd $GIT_DIR
-git add -A > /dev/null 2>&1
-git commit -m "auto commit `date +%Y-%m-%dT%H:%M:%S%z`" 2>&1
+git add -A > /dev/null
+git commit -m "auto commit `date +%Y-%m-%dT%H:%M:%S%z`"
 GIT_SSH=./git-ssh.sh git pull
 GIT_SSH=./git-ssh.sh git push
