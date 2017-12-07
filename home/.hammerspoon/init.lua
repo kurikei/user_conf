58c9fa13081c8534494beddf152e6f394af3f8fb
kurikei <kurikei@gmail.com>
2017-12-07 18:28:02 +0900

    add hammerspoon shortcut

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,13 +1,53 @@
+hs.hotkey.bind({"control"}, "a", function()
+  hs.application.launchOrFocus("Astro")
+end)
+
+hs.hotkey.bind({"control"}, "d", function()
+  hs.application.launchOrFocus("Dash")
+end)
+
+hs.hotkey.bind({"control"}, "e", function()
+  hs.application.launchOrFocus("Evernote")
+end)
+
+hs.hotkey.bind({"control"}, "f", function()
+  hs.application.launchOrFocus("Firefox")
+end)
+
+hs.hotkey.bind({"control"}, "g", function()
+  hs.application.launchOrFocus("Goland")
+end)
+
+hs.hotkey.bind({"control"}, "k", function()
+  hs.application.launchOrFocus("Kindle")
+end)
+
 hs.hotkey.bind({"control"}, "i", function()
   hs.application.launchOrFocus("iTerm")
 end)
 
-hs.hotkey.bind({"control"}, ",", function()
-  hs.application.launchOrFocus("Google Chrome")
+hs.hotkey.bind({"control"}, "o", function()
+  hs.application.launchOrFocus("1Password 6")
+end)
+
+hs.hotkey.bind({"control"}, "p", function()
+  hs.application.launchOrFocus("Postman")
 end)
 
 hs.hotkey.bind({"control"}, "s", function()
   hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind({"control"}, "t", function()
+  hs.application.launchOrFocus("Things")
+end)
+
+hs.hotkey.bind({"control"}, "x", function()
+  hs.application.launchOrFocus("Xcode")
+end)
+
+hs.hotkey.bind({"control"}, ",", function()
+  hs.application.launchOrFocus("Google Chrome")
 end)
 
 local function remapKey(mods, key, keyCode)
